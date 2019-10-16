7b2b1428e762072bea43163ef4c49667eba05c0c
Ishan Mishra <ishan2198@hotmail.com>
2019-10-17 01:18:48 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
 # Think to manipulate this using GUI
-frame1 = 400
-frame2 = 800
-
+from GuiForFrame import counter,counter_2
+frame1 = counter.get()
+frame2 = counter_2.get()
+print(counter.get())
+print(counter_2.get())
 
 # Define the region of interest polygon here
 # Improvement:
